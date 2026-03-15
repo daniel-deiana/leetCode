463efee01b1b1920b0b31cca4bfc8204689f58fd
daniel-deiana <deiana.studio1@gmail.com>
2026-03-15 14:41:46 +0100

    (feat): add longestCommonSubsequence

---
+++ dynamicProgramming/longestCommonSubsequence.py
@@ -0,0 +1,10 @@
+"""
+Given two strings text1 and text2, return the length of the longest common subsequence between the two strings if one exists, otherwise return 0.
+
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the relative order of the remaining characters.
+
+    For example, "cat" is a subsequence of "crabt".
+    
+A common subsequence of two strings is a subsequence that exists in both strings
+
+"""
